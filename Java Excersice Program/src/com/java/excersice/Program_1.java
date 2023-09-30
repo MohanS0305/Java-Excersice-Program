2c94cc8409253f33bff34ae2a3fc155826ae8614
Mohan <smohan0305@gmail.com>
2023-09-30 15:13:28 +0530

    first commit

---
+++ Java Excersice Program/src/com/java/excersice/Program_1.java
@@ -0,0 +1,22 @@
+package com.java.excersice;
+
+import java.util.Scanner;
+
+/*  1. Write a Java program to convert temperature from Fahrenheit to Celsius degree.
+		Test Data
+		Input a degree in Fahrenheit: 212
+		Expected Output:
+		212.0 degree Fahrenheit is equal to 100.0 in Celsius */
+
+public class Program_1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter any no :");
+		int f = input.nextInt();
+		double c = ((f-32)*5)/9;
+		System.out.println(f+" degree fahrenheit is equal to "+c+" in celsius");
+		input.close();
+		
+		}
+}
---
+++ Java Excersice Program/src/com/java/excersice/Program_3.java
@@ -0,0 +1,26 @@
+package com.java.excersice;
+
+import java.util.Scanner;
+
+/* 3. Write a Java program to compute body mass index (BMI).
+		Test Data
+		Input weight in pounds: 452
+		Input height in inches: 72
+		Expected Output:
+		Body Mass Index is 61.30159143458721 */
+
+public class Program_3 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the weight in pounds:");
+		double weight = input.nextDouble();
+		System.out.println("Enter the height in centimeter:");
+		double height = input.nextDouble();
+		
+		double bmi = weight*0.4535/(height*0.0254*height*0.0254);
+		System.out.println("Your bmi is : "+bmi);
+		input.close();
+	}
+
+}
